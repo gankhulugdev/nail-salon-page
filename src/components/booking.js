7e7c7e0b3ba866d976089d7effdd97fa885386c4
Hulguu-icodice <hulguu@icodice.com>
2023-10-16 23:27:24 -0500

    button with background

--- src/components/booking.js
+++ src/components/booking.js
@@ -11,8 +11,8 @@ export default function Booking() {
         id='book' className="px-2 sm:px-4 md:px-8 lg:px-16 flex justify-center  ">
 
 
-        <div id="book-button" className="hover:cursor-pointer hover:border-b-2 ">
-            <a href="https://www.fresha.com/book-now/lamour-spa-bgtfzb1t/services?lid=100600&pId=93395" className="pt-2 md:pt-6 lg:pt-8 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+        <div id="book-button">
+            <a href="https://www.fresha.com/book-now/lamour-spa-bgtfzb1t/services?lid=100600&pId=93395" className="border bg-salongreen/40 rounded-2xl px-6 text-2xl font-bold tracking-tight text-black-900 sm:text-4xl">
                 Book Now
             </a>
         </div>
--- src/components/gift-card.js
+++ src/components/gift-card.js
@@ -6,8 +6,8 @@ export default function GiftCard() {
         animate={{ opacity: 1 }}
         transition={{ duration: 0.75, ease: "easeOut" }}
         id='gift-card' className="px-2 sm:px-4 md:px-8 lg:px-16 flex justify-center ">
-        <div id="gift-card-button" className="hover:cursor-pointer">
-            <a href="https://www.fresha.com/book-now/lamour-spa-bgtfzb1t/gift-cards?pId=93395" className="pt-2 md:pt-6 lg:pt-8 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+        <div id="gift-card-button">
+            <a href="https://www.fresha.com/book-now/lamour-spa-bgtfzb1t/gift-cards?pId=93395" className=" bg-white/60 rounded-2xl px-6 text-2xl font-bold tracking-tight text-black-900 sm:text-4xl">
                 Buy Gift Card Here
             </a>
         </div>
